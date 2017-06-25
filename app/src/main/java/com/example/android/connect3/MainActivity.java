b4c16d8ce34ee4848a131f0cff57224e8812616f
SuperHaker <amrits3624@gmail.com>
2017-06-25 06:21:49 +0530

    Tokens and animate

--- app/src/main/java/com/example/android/connect3/MainActivity.java
+++ app/src/main/java/com/example/android/connect3/MainActivity.java
@@ -1,9 +1,20 @@
 package com.example.android.connect3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void dropIn(View view){
+
+        ImageView token = (ImageView) view;
+        token.setTranslationY(-1000f);
+        token.setImageResource(R.drawable.yellow);
+        token.animate().translationYBy(1000f).rotation(360).setDuration(300);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
